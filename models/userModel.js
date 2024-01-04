c62a0ca9121b66add1bd7439a70abf47f1240c71
chitra0404 <chitrasuresh0404@gmail.com>
2024-01-04 17:30:49 +0530

    first

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    account_activated:{
+        type:Boolean,
+        default:false
+    },
+    token_activate_account:{
+        type:String
+    },
+    token_reset_password:{
+        type:String
+    },
+    urlshortener:{
+        
+    }
+})
+
+module.exports=mongoose.model('user',userSchema);
---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require('cors');
+const route=require('./routes/userRoute');
+const app=express();
+require('dotenv').config();
+app.use(cors);
+app.use(express.json());
+app.use(route);
+
+//atlas Url
+const url=process.env.ATLAS_URL;
+//mongoose connect
+mongoose.connect(url)
+.then(()=>console.log("connect to mongodb"))
+.catch(err=>console.log("error occured",err));
+const port=8080;
+app.listen(port,()=>console.log(`listening to port ${port}`));
